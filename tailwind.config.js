64d3db5940abbf0903550061902215f2ec2da4d6
felipelifa <felipe.delifa@gmail.com>
2025-07-12 15:16:18 -0300

    ATT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ajuste conforme sua estrutura
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
