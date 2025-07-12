f4c2a57a0ce1ccb79ed8ddb9e45cc36c67f3e0e0
felipelifa <felipe.delifa@gmail.com>
2025-07-12 14:38:45 -0300

    att

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // outros caminhos se precisar
-  ],
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
